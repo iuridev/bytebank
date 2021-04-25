4600d2c3469898cddd2423df5dd15a4cfd51fd21
iuridev <iuribarreto.ib@gmail.com>
2021-04-25 19:40:39 -0300

    Estudo com Herança

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,23 @@
-     fun main(){
-         println("Bem vindo ao Bytebank")
-         val name = "iuri"
-         println("Titular $name")
-     }
+fun main(){
+    println("Bem vindo ao Bytebank")
+    val name = "iuri"
+    println("Titular $name")
+
+    //criando funcionario
+    val iuri = Funcionario( "Iuri", "123.456.789-10", 1500.0)
+    println("Nome: ${iuri.nome}")
+    println("CPF:: ${iuri.cpf}")
+    println("Salário: ${iuri.salario}")
+    println("Bonificação: ${iuri.bonificacao}")
+    println("Função: ${iuri.tipo}")
+
+
+    //criando Gerente
+    val maria = Gerente("Maria", "125.584.586-87", 3500.0)
+    println("Nome: ${maria.nome}")
+    println("CPF:: ${maria.cpf}")
+    println("Salário: ${maria.salario}")
+    println("Bonificação: ${maria.bonificacao}")
+    println("Função: ${maria.tipo}")
+
+}
